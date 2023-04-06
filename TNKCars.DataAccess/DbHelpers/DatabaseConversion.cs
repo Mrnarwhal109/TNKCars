0aeafc29165074a9b3496bdadc0084e7f6f942dc
Mrnarwhal109 <jediknight1091@gmail.com>
2023-04-06 16:43:24 -0500

    The custom command has been developed further.
    The client project has been refactored.

--- TNKCars.DataAccess/DbHelpers/DatabaseConversion.cs
+++ TNKCars.DataAccess/DbHelpers/DatabaseConversion.cs
@@ -8,5 +8,29 @@ namespace TNKCars.DataAccess.DbHelpers
 {
     internal static class DatabaseConversion
     {
+        internal async static Task<List<Car>> RowsToCars(TableResult tableRows)
+        {
+            List<Car> cars = new List<Car>();
+            List<Task> activeWork = new List<Task>();
+
+            foreach (var row in tableRows.Data)
+            {
+                activeWork.Add(RowToCar(row));               
+            }
+
+            await Task.WhenAll(activeWork);
+
+            return cars;
+        }
+
+        internal static async Task<Car> RowToCar(List<object> dataRow)
+        {
+            var result = await Task.Run(() =>
+            {
+                Car car = new Car();
+                return car;
+            });
+            return result;
+        }
     }
 }
---
+++ TNKCars.DataAccess/DbHelpers/DatabaseUtility.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TNKCars.DataAccess.DbHelpers
+{
+    internal static class DatabaseUtility
+    {
+        public static string GetConnectionString()
+        {
+            // Hard coded for now
+            //string user = "postgres";
+            //string password = "password";
+            return FormatConnectionString();
+        }
+
+        public static string FormatConnectionString()
+        {
+            return $"postgresql://localhost";
+        }
+
+        public static string FormatConnectionString(ref string username, 
+            ref string password, ref string networkLocation, ref string port, ref string dbName)
+        {
+            // postgresql://[user[:password]@][netloc][:port][/dbname][?param1=value1&...]
+            return $"postgresql://localhost";
+        }
+    }
+}
