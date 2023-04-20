90900e382ad40797998f06c12cdf92433d51a142
Mrnarwhal109 <jediknight1091@gmail.com>
2023-04-20 16:30:47 -0500

    Added queries and row conversion methods.

--- TNKCars.DataAccess/DbHelpers/DAOCars.cs
+++ TNKCars.DataAccess/DbHelpers/DAOCars.cs
@@ -10,18 +10,38 @@ namespace TNKCars.DataAccess.DbHelpers
 {
     public static class DAOCars
     {
-        async internal static Task<TableResult> GetAllJoinedCarData(NpgsqlConnection connection)
+        async public static Task<List<Car>> GetAllCarsWithDetails(NpgsqlConnection connection)
         {
             var command = await CustomDbCommand.CreateAsync(connection);
-            var dbResult = await command.ReaderResult(QueryDefinitions.GetAllJoinedCarData);
-            return dbResult;
-        }
-
-        async public static Task<List<Car>> GetAllCars(NpgsqlConnection connection)
-        {
-            var tableRows = await GetAllJoinedCarData(connection);
-            var converted = await DatabaseConversion.JoinedCarDataToCars(tableRows);
+            var dbResult = await command.ReaderResult(QueryDefinitions.GetAllJoinedCars);
+            var converted = await DatabaseConversion.JoinedCarDataToCars(dbResult);
             return converted;
         }
+
+        async public static Task<List<Car>> GetAllCarsWIthoutDetails(NpgsqlConnection connection)
+        {
+            var command = await CustomDbCommand.CreateAsync(connection);
+            var dbResult = await command.ReaderResult(QueryDefinitions.GetAllCars);
+            var converted = await DatabaseConversion.CarDataToCars(dbResult);
+            return converted;
+        }
+
+        async public static Task<List<Engine>> GetAllEngines(NpgsqlConnection connection)
+        {
+            var command = await CustomDbCommand.CreateAsync(connection);
+            var dbResult = await command.ReaderResult(QueryDefinitions.GetAllEngines);
+            var converted = await DatabaseConversion.EngineDataToEngines(dbResult);
+            return converted;
+        }
+
+        async public static Task<List<Transmission>> GetAllTransmissions(NpgsqlConnection connection)
+        {
+            var command = await CustomDbCommand.CreateAsync(connection);
+            var dbResult = await command.ReaderResult(QueryDefinitions.GetAllTransmissions);
+            var converted = await DatabaseConversion.TransmissionDataToTransmissions(dbResult);
+            return converted;
+        }
+
+
     }
 }
